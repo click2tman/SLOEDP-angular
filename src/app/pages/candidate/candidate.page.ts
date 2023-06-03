0d67d0319271ce600e0ea275f1759ce60f2a0e10
Hassan Ali <hali6338@gmail.com>
2023-06-04 01:42:56 +0500

    Show candidate.

--- src/app/pages/candidate/candidate.page.ts
+++ src/app/pages/candidate/candidate.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-candidate',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./candidate.page.scss'],
 })
 export class CandidatePage implements OnInit {
-
-  constructor() { }
+	public candidate: any;
+  constructor(
+    private commonService: CommonService,
+    private route: ActivatedRoute
+  ) { 
+    const id = this.route.snapshot.params['id'];
+    console.log(id);
+    this.candidate = commonService.getCandidate(id)
+  }
 
   ngOnInit() {
   }
+  back(){
+    this.commonService.back();
+  }
 
+	sourceUrl(url: string) {
+		return "assets/imgs/candidate/" + url;
+	}
+
+	getCandidateName() {
+		return this.candidate.Prefix + " " + this.candidate.FirstName + " " + this.candidate.MiddleName + " " + this.candidate.SurName;
+	}
 }
